c92276d8c46e3cda22a19d08daa3bebbf9611a8c
Jacob Nathaniel Wessell <jwessell6@atl1-1-01-005-11-0.pace.gatech.edu>
2024-11-18 09:49:31 -0500

    re-factored code in step files and added a pipeline to generate embeddings en-masse

--- RAG_Pipeline/step2_csv_to_FAISS_embedding.py
+++ RAG_Pipeline/step2_csv_to_FAISS_embedding.py
@@ -5,74 +5,62 @@ from numpy.linalg import norm
 import cv2
 import pandas as pd
 from tqdm import tqdm
-from transformers import BridgeTowerProcessor, BridgeTowerForContrastiveLearning
 from PIL import Image
 from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
 import torch
 from PIL import Image
 import requests
 from termcolor import cprint
-
+import faiss
 import argparse
 
-# Create the parser
-parser = argparse.ArgumentParser(description="parser for step2")
-
-# Define expected arguments
-parser.add_argument('--i', type=str, help='path to the input folder')
-
-
-# Parse the arguments
-args = parser.parse_args()
-
-
-INPUT_PATH = args.i
-#should have frames folder, and the csv
-
-
-
-
-processor = BridgeTowerProcessor.from_pretrained("BridgeTower/bridgetower-large-itm-mlm-itc")
-model = BridgeTowerForContrastiveLearning.from_pretrained("BridgeTower/bridgetower-large-itm-mlm-itc")
-
-
-def encode_single_image_text(image_path, text):
-    print(image_path)
-    print(text)
-    image = Image.open(image_path)
-    encoding = processor(image, text, return_tensors="pt")
-    outputs = model(**encoding)
-    return outputs['cross_embeds'].detach().numpy()
 import pandas as pd
 
-#load ./paired_image_data.csv
-df = pd.read_csv(f'{INPUT_PATH}/image_transcript_mapping.csv')
-#rows :             index,image_path,transcript_text,timestamp
+def encode_single_image_text(image_path, text, processor, model):
+    image = Image.open(image_path)
+    encoding = processor(image, text, return_tensors="pt").to("cuda:0")
+    outputs = model(**encoding)
+    return outputs['cross_embeds'].cpu().detach().numpy()
 
-#create empty FAISS index
-import faiss
-
-# Create embeddings for each row
-embeddings = np.zeros((len(df), 512))
-for _, row in tqdm(df.iterrows()):
+#moved model and processor to main function call to save time on loading/reloading model and sending weights to GPU
+def gen_embeddings(path: str, csv:str, model, processor):
     
+    #load ./paired_image_data.csv
+    df = pd.read_csv(f'{path}/{csv}')
+    #rows :             index,image_path,transcript_text,timestamp
 
-    embedding = encode_single_image_text(row['image_path'], row['transcript_text'])
-    #reshape to (512,)
-    embedding = embedding.reshape(512)
-    embeddings[row['index'],:] = embedding
+    #create empty FAISS index
+
+
+    # Create embeddings for each row
+    embeddings = np.zeros((len(df), 512))
+    for _, row in tqdm(df.iterrows()):
+        embedding = encode_single_image_text(row['image_path'], row['transcript_text'], processor, model)
+        #reshape to (512,)
+        embedding = embedding.reshape(512)
+        embeddings[row['index'],:] = embedding
 
     print(embedding.shape)
 
-# Initialize FAISS index
-dimension = 512  # dimension of your embeddings
-index = faiss.IndexFlatL2(dimension)
+    # Initialize FAISS index
+    dimension = 512  # dimension of your embeddings
+    index = faiss.IndexFlatL2(dimension)
 
-# Add embeddings to the FAISS index
-index.add(embeddings)
+    # Add embeddings to the FAISS index
+    index.add(embeddings)
 
-# Create a mapping between FAISS ids and dataframe indices
-id_to_index = {i: idx for i, idx in enumerate(df.index)}
+     #Create a mapping between FAISS ids and dataframe indices
+    id_to_index = {i: idx for i, idx in enumerate(df.index)}
 
-# Save the FAISS index
-faiss.write_index(index, f"{INPUT_PATH}/faiss_database.bin")
+    # Save the FAISS index
+    faiss.write_index(index, f"{path}/faiss_database.bin")
+
+if __name__ == "__main__":
+    # Create the parser
+    parser = argparse.ArgumentParser(description="parser for step2")
+    # Define expected arguments
+    parser.add_argument('--i', type=str, help='path to the input folder')
+    parser.add_argument('--n', type=str, help='name of target csv file')
+    # Parse the arguments
+    args = parser.parse_args()
+    gen_embeddings(args.i)
--- RAG_Pipeline/step3_FAISS_augmented_chat.py
+++ RAG_Pipeline/step3_FAISS_augmented_chat.py
@@ -13,59 +13,46 @@ import torch
 from PIL import Image
 import requests
 from termcolor import cprint
-
+from transformers import LlavaNextForConditionalGeneration, BitsAndBytesConfig
 #import return_image_and_enhanced_query function from ./query.py
 
 from query import return_image_and_enhanced_query
-
-
 import argparse
 
 # Create the parser
-parser = argparse.ArgumentParser(description="parser for step3")
 
-# Define expected arguments
-parser.add_argument('--i', type=str, help='path to the input folder')
-parser.add_argument('--q', type=str, help='query string')
-
-args = parser.parse_args()
-
-QUERY = args.q
+def RAG_question(path, question):
+    QUERY = args.q
+    conv,image = return_image_and_enhanced_query(QUERY, args.i)
 
 
-conv,image = return_image_and_enhanced_query(QUERY, args.i)
+    ########################## LOADING LLAVA ##########################
+    #clear cuda
+    torch.cuda.empty_cache()
 
+    print("beginning to load LLaVA")
+    #load on cuda:0
 
-########################## LOADING LLAVA ##########################
-#clear cuda
-torch.cuda.empty_cache()
+    llava_processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", device="cuda:0")
 
-print("beginning to load LLaVA")
-#load on cuda:0
+    # specify how to quantize the model
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16,
+    )
 
-llava_processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", device="cuda:0")
-
-
-from transformers import LlavaNextForConditionalGeneration, BitsAndBytesConfig
-
-# specify how to quantize the model
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
-)
-
-llava_model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", quantization_config=quantization_config, device_map="auto")
+    llava_model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", quantization_config=quantization_config, device_map="auto")
 
 
 
-prompt = llava_processor.apply_chat_template(conv, add_generation_prompt=True)
-inputs = llava_processor(image,prompt, return_tensors="pt").to("cuda:0")
+    prompt = llava_processor.apply_chat_template(conv, add_generation_prompt=True)
+    inputs = llava_processor(image,prompt, return_tensors="pt").to("cuda:0")
 
-# autoregressively complete prompt
-output = llava_model.generate(**inputs, max_new_tokens=200)
+    # autoregressively complete prompt
+    output = llava_model.generate(**inputs, max_new_tokens=200)
 
-print(f"enhanced answer:{llava_processor.decode(output[0], skip_special_tokens=True)}")
+    print(f"enhanced answer:{llava_processor.decode(output[0], skip_special_tokens=True)}")
 
 
 # start_secs = time.time()
@@ -80,3 +67,11 @@ print(f"enhanced answer:{llava_processor.decode(output[0], skip_special_tokens=T
 #     output = llava_model.generate(**inputs, max_new_tokens=100)
 
 #     cprint(f"enhanced answer:{llava_processor.decode(output[0], skip_special_tokens=True)}",on_color='on_red')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="parser for step3")
+
+    # Define expected arguments
+    parser.add_argument('--i', type=str, help='path to the input folder')
+    parser.add_argument('--q', type=str, help='query string')
+
+    args = parser.parse_args()
