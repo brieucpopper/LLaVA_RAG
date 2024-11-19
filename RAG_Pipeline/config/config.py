8ca70ebda92b3fbd60a1bc43f99642d04463599e
Brandon Yicheng Zhou <bzhou91@atl1-1-01-002-8-0.pace.gatech.edu>
2024-11-18 20:19:48 -0500

    add tsc sampling

---
+++ RAG_Pipeline/config/config.py
@@ -0,0 +1,3 @@
+divlam     = 24 #use to adjust threshold
+num_picks  = 1 #frames to keep from each "cluster"
+img_folder = "/home/hice1/bzhou91/scratch/LLaVA_RAG_PoC/test_pipeline_withparser/frames" # img folder path
